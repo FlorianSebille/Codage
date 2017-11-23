068685ce1be11abe6fe7c3579f92f125e6eda0c6
WarNN <muctutur72@hotmail.fr>
2017-11-23 17:58:09 +0100

    Modif

--- PseudoAleatoire/main.c
+++ PseudoAleatoire/main.c
@@ -1,6 +1,8 @@
 #include "codeLM.h"
 #include "codeGold.h"
 #include "codeJPL.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 
 
@@ -8,8 +10,10 @@ int main(){
 
   int vecteur1[4] = {5,4,2,1};
   int vecteur2[2] = {5,2};
+  int nb_elem_vecteur1 = (int)(sizeof(vecteur1)/sizeof(int));
+  int nb_elem_vecteur2 = (int)(sizeof(vecteur2)/sizeof(int));
   int tab[L];
 
-  actionGold(vecteur1,4,vecteur2,2,tab);
+  actionGold(vecteur1,nb_elem_vecteur1,vecteur2,nb_elem_vecteur2,tab);
 
 }
